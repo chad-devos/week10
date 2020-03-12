90425a0f1aaa1826b5f2e5992f302e8471e97da2
chad-devos <chad.devos@kellogg.northwestern.edu>
2020-03-12 01:01:50 +0000

    ready for heroku

--- app.rb
+++ app.rb
@@ -65,7 +65,7 @@ post "/events/:id/rsvps/create" do
         comments: params["comments"],
         going: params["going"]
     )
-    view "create_rsvp"
+   redirect "events/#{@event[:id]}"
 end
 
 get "/rsvps/:id/edit" do
@@ -79,7 +79,17 @@ end
 post "/rsvps/:id/update" do
     puts "params: #{params}"
 
+    @rsvp = rsvps_table.where(id: params["id"]).to_a[0]
+    @event = events_table.where(id: @rsvp[:event_id]).to_a[0]
+    if @current_user && @current_user[:id] == @rsvp[:id]
+        rsvps_table.where(id: params["id"]).update(
+         going: params["going"],
+            comments: params["comments"]
+        )
     view "update_rsvp"
+    else 
+        view "error.erb"
+    end 
 end
 
 get "/rsvps/:id/destroy" do
@@ -102,12 +112,17 @@ end
 post "/users/create" do
     puts "params: #{params}"
 
+    existing_user = users_table.where(email: params["email"]).to_a[0]
+    if existing_user
+        view "error"
+    else
     users_table.insert(
         name: params["name"],
         email: params["email"],
         password: BCrypt::Password.create(params["password"])
     )
     view "create_user"
+    end
 end
 
 # display the login form (aka "new")
@@ -139,5 +154,5 @@ end
 get "/logout" do
     # remove encrypted cookie for logged out user
     session["user_id"] = nil
-    view "logout"
+    redirect "/logins/new"
 end
